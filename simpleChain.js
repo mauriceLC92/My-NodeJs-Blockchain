2637f45a11060dc422309792725295da71899064
Maurice <mauricelecordier@gmail.com>
2019-01-14 22:30:31 +0200

    fleshed out addBlock method

--- simpleChain.js
+++ simpleChain.js
@@ -1,4 +1,4 @@
-const SHA256 =  require('crypto-js').SHA256;
+const SHA256 = require('crypto-js').SHA256;
 
 // Class with a constructor for block data model
 class Block {
@@ -23,9 +23,20 @@ class Block {
 class Blockchain {
     constructor() {
         this.chain = [];
+        this.addBlock(this.createGenesisBlock());
     }
 
-    addBlock(newBlock){
+    createGenesisBlock() {
+        return new Block('Genesis Block');
+    }
+
+    addBlock(newBlock) {
+        newBlock.height = this.chain.length;
+        newBlock.timeStamp = new Date().getTime().toString().slice(0, -3); // Compatible UTC timestamp
+        if (this.chain.length > 0) {
+            newBlock.previousblockhash = this.chain[this.chain.length - 1].hash;
+        }
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString(); // SHA256 requires a string of data
         this.chain.push(newBlock);
     }
 }
@@ -33,13 +44,14 @@ class Blockchain {
 
 
 
-// const block1 = new Block('I am block 1');
-// const block2 = new Block('I am block 2');
+const block1 = new Block('I am block 1');
+const block2 = new Block('I am block 2');
 
-// const mauriceChain = new Blockchain();
-
-// mauriceChain.addBlock(block1);
-// console.log('mauriceChain', mauriceChain );
-// console.log('---------------------')
-// mauriceChain.addBlock(block2);
-// console.log('mauriceChain', mauriceChain );
+const mauriceChain = new Blockchain();
+console.log('mauriceChain', mauriceChain );
+console.log('---------------------')
+mauriceChain.addBlock(block1);
+console.log('mauriceChain', mauriceChain );
+console.log('---------------------')
+mauriceChain.addBlock(block2);
+console.log('mauriceChain', mauriceChain );
